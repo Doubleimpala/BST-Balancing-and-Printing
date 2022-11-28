892b6fcf114931a9f597b5380003e60119e8970f
Tanmay Garudadri <>
2022-11-28 17:05:51 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    BST<Integer> tree = new BST<Integer>();
+    tree.insert(2);
+    tree.insert(3);
+    tree.insert(7);
+    tree.printTree();
+  }
+}
